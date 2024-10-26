fec83d09050b74498e9c00bb7409e470bddd7f42
Caio Vellani <caiovellani@outlook.com>
2024-10-26 19:15:21 -0300

    feat: add CyclesContext Provider

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,10 +1,14 @@
-import { HistoryContainer, HistoryList, Status } from "./styles";
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useContext } from "react"
+import { CyclesContext } from "../../contexts/CyclesContext"
+import { HistoryContainer, HistoryList, Status } from "./styles"
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
     return (
         <HistoryContainer>
             <h1>Meu Histórico</h1>
-
             <HistoryList>
                 <table>
                     <thead>
@@ -16,14 +20,30 @@ export function History() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluido</Status>
-                            </td>
-                        </tr>
+                        {cycles.map((cycle) => {
+                          return (
+                            <tr key={cycle.id}>
+                              <td>{cycle.task}</td>
+                              <td>{cycle.minutesAmount} minutos</td>
+                              <td>{formatDistanceToNow(cycle.startDate, {
+                                addSuffix: true,
+                                locale: ptBR
+                              })}
+                              </td>
+                              <td>
+                                { cycle.finishedDate && (
+                                  <Status statusColor="green">Concluido</Status>
+                                )}
+                                { cycle.interruptedDate && (
+                                  <Status statusColor="red">Interrompido</Status>
+                                )}
+                                { (!cycle.finishedDate && !cycle.interruptedDate) && (
+                                  <Status statusColor="yellow">Em andamento</Status>
+                                )}
+                              </td>
+                            </tr>
+                          )
+                        })}
                     </tbody>
                 </table>
             </HistoryList>
--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,13 +1,11 @@
 import { differenceInSeconds } from "date-fns";
 import { useContext, useEffect, } from "react";
-import { CyclesContext } from "../..";
+import { CyclesContext } from "../../../../contexts/CyclesContext";
 import { CountdownContainer, Separator } from "./styles";
 
 export function Countdown() {
     const { activeCycle, activeCycleId, markCurrentCycleAsFinished, amountSecondsPassed, setSecondsPassed } = useContext(CyclesContext)
-    
-
-     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
     useEffect(() => {
      let interval: number
